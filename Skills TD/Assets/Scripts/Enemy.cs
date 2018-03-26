1374fbe019021de44c5dbd2acaa8a836a5cee070
Ashton Ortiz <36577221+ENLSITEDbeast@users.noreply.github.com>
2018-03-25 22:46:02 -0700

    MORE PROGRESS

---
+++ Skills TD/Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int waypointIndex = 0;
+
+	void Start ()
+	{
+		target = Waypoints.WayPoints[0];
+	}
+
+	void Update ()
+	{
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+	
+	
+	
+	}
+
+
+}
---
+++ Skills TD/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour {
+
+	public static Transform[] WayPoints;
+
+	void Awake ()
+	{
+		WayPoints = new Transform[transform.childCount];
+		for (int i = 0; i < WayPoints.Length; i++) 
+		{
+			WayPoints[i] = transform.GetChild(i);
+		}
+	}
+}
